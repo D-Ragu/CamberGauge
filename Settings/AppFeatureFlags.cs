27c1353f1aa6632d30c295a56f1ae733011dc52d
DR <draguseo99@gmail.com>
2026-04-24 21:03:29 -0400

    QOL

--- Settings/AppFeatureFlags.cs
+++ Settings/AppFeatureFlags.cs
@@ -4,4 +4,5 @@ public static class AppFeatureFlags
 {
     // Flip this to false when testing on a real phone.
     public const bool UseMockSensorData = true;
+    //public const bool UseMockSensorData = false;
 }
